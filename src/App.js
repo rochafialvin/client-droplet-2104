bde2b67981698145a938deab645fc534982ec12f
rochafialvin <rochafi.dev@gmail.com>
2022-08-03 11:07:53 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:7000/users");
+      setUsers(response.data.data.result);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const renderUsers = () =>
+    users.map((user) => (
+      <li>
+        {user.user_id}. {user.username}
+      </li>
+    ));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>List of user</p>
+        <ul>{renderUsers()}</ul>
         <a
           className="App-link"
           href="https://reactjs.org"
